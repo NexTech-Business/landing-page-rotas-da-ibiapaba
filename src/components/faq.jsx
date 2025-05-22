eb505ff41c2bc2fc4acfa0398f91ffb317d76b54
Nécio de Lima Veras <necioveras@gmail.com>
2025-05-22 15:57:29 -0300

    FAQs revisado

--- src/components/faq.jsx
+++ src/components/faq.jsx
@@ -10,34 +10,34 @@ const FAQ = () => {
 				"Você pode acessar usando seu celular ou computador para gerenciar as suas vendas e pedidos. Seus clientes poderão comprar a partir de qualquer dispositivo ou por meio de um dos nossos totens espalhados pelos estabelecimentos parceiros ou em pontos turísticos estratégicos.",
 		},
 		{
-			pergunta: "O cliente precisa estar no estabelecimento para comprar?",
+			pergunta: "Meu cliente precisa estar no meu estabelecimento para comprar?",
 			resposta:
-				"Não. O cliente pode comprar produtos de qualquer parceiro através um link ou em Totens, o que facilita o acesso aos seus produtos e aumenta o alcance do seu negócio.",
+				"Não. Seu cliente poderá comprar produtos de qualquer parceiro através um link ou em um Totem, o que facilita o acesso aos seus produtos e aumenta o alcance do seu negócio.",
 		},
 		{
 			pergunta: "Quais tipos de produtos podem ser vendidos?",
 			resposta:
-				"A plataforma permite a venda de produtos, serviços, ingressos e outros através de tickets de acesso, podendo valer produtos típicos da região, como ingressos para atrações, artesanato, souvenirs, passeios e outros itens turisticos locais.",
+				"A plataforma permite a venda de produtos, serviços, ingressos e outros através de tickets, podendo incluir produtos típicos da região, como ingressos/entradas para atrações, artesanato, souvenirs, passeios e outros itens turisticos locais.",
 		},
 		{
-			pergunta: "Como os clientes sabem quais locais possuem Totens?",
+			pergunta: "Como meus clientes sabem quais locais possuem Totens?",
 			resposta:
-				"Todos os estabelecimentos parceiros com Totens estão listados no nosso site. Isso ajuda o cliente a encontrar facilmente onde comprar seus produtos.",
+				"Todos os estabelecimentos parceiros com Totens estão listados no nosso site. Isso ajuda seu cliente a encontrar facilmente onde comprar seus produtos.",
 		},
 		{
 			pergunta: "Quais formas de pagamento são aceitas?",
 			resposta:
-				"Por enquanto, apenas pagamentos em Pix, oferecendo praticidade para os clientes e mais possibilidades de venda para o seu negócio.",
+				"Por enquanto, apenas pagamentos em Pix, oferecendo praticidade para seus clientes e mais possibilidades de venda para o seu negócio.",
 		},
 		{
 			pergunta: "Sou dono de um estabelecimento. Como posso participar?",
 			resposta:
-				"Basta se cadastrar pelo nosso site. O processo é simples e rápido. Após o cadastro, em até 48h estaremos entrando em contato.",
+				"Basta se cadastrar pelo nosso site. O processo é simples e rápido. Após o cadastro, em até 48h entraremos em contato com você.",
 		},
 		{
 			pergunta: "Como funciona a integração dos meus produtos na plataforma e nos Totens?",
 			resposta:
-				"Você cadastra seus produtos na plataforma e eles ficam automaticamente disponíveis nos Totens dos parceiros mais próximos, conforme o plano que você contratar.",
+				"Você cadastra seus produtos na plataforma e eles automaticamente ficarão disponíveis em todos os Totens, isto é, seus produtos serão listados nos Totens presentes nos parceiros mais próximos, conforme o plano que você contratar.",
 		},
 		{
 			pergunta: "Quem define os preços dos produtos?",
